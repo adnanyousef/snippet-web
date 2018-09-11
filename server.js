0df19f101023fc2eeff7edec4ebf032e00c408ef
Adnan Yousef <yousef.eddy@gmail.com>
2018-09-10 20:22:55 -0500

    Added sync force

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ if (process.env.SETUP_DB) {
 };
 
 // Listener
-db.sequelize.sync(syncOptions).then(function() {
+db.sequelize.sync({force: true}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
